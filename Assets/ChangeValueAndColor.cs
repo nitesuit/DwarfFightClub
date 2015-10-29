325583f13120b9036aa3d8d9fdd0c0667727faaf
Povilas Staskus <povilas.staskus@gmail.com>
2015-10-29 16:23:20 +0200

    life did again

---
+++ Assets/ChangeValueAndColor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeValueAndColor : MonoBehaviour {
+
+	private LifeController lc;
+	private Slider slider;
+	public Image image;
+	// Use this for initialization
+	void Awake () {
+		lc = GetComponentInParent<LifeController> ();
+		slider = GetComponent<Slider> ();
+
+		slider.maxValue = lc.lives;
+		slider.value = lc.lives;
+		slider.minValue = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		slider.value = lc.lives;
+		Debug.Log (slider.value);
+	
+	}
+
+	public void ChangeColor(Slider slider) {
+
+		image.color = new Color (Mathf.Abs (1 - slider.value/slider.maxValue), slider.value / slider.maxValue, image.color.b, image.color.a);
+
+
+	}
+}
