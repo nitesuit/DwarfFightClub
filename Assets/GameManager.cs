833282a9d1e80f5f30157e1f965264be074e385f
Povilas Staskus <povilas.staskus@gmail.com>
2015-11-24 12:34:54 +0200

    part of the menu

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject.DontDestroyOnLoad (gameObject);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
